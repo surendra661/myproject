a516f724a91b48f4fc73ea39a74e8568b4d622b6
shaikmustafa77 <awsanddevops37@gmail.com>
2025-02-13 12:28:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// You can add interactivity here if needed
+// Example: Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
